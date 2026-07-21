d93f97fb25fd2493ef7468b51b213d7f0122ce94
Lê Ngọc Anh Quân <lnaq.1406.work@gmail.com>
2026-07-21 10:57:08 +0900

    Add Decorator

---
+++ DesignPatternExamples/Program.cs
@@ -0,0 +1,41 @@
+﻿//Tạo interface
+interface IMessage
+{
+    void Send();
+}
+
+//Tạo component
+class EmailMessage : IMessage
+{
+    public void Send()
+    {
+        Console.WriteLine("Send Email");
+    }
+}
+
+//Tạo decorator
+class LogDecorator : IMessage
+{
+    private readonly IMessage message;
+
+    public LogDecorator(IMessage message)
+    {
+        this.message = message;
+    }
+
+    public void Send()
+    {
+        Console.WriteLine("Write Log");
+        message.Send();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IMessage message = new LogDecorator(new EmailMessage());
+
+        message.Send();
+    }
+}
